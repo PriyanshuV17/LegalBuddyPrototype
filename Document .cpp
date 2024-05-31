49a5b77f7717b2afac4567b84cd79eb7987f0a20
PRIYANSHU VERMA <priyansgood392@gmail.com>
2024-05-31 14:27:29 +0530

    Create Document .cpp

---
+++ Document .cpp
@@ -0,0 +1,7 @@
+#include "Document.h"
+
+Document::Document(const std::string& title, const std::string& content) : title(title), content(content) {}
+
+std::string Document::getTitle() const { return title; }
+
+std::string Document::getContent() const { return content; }
