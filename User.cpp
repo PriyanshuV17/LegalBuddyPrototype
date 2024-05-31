c82d85b11869bf83f6afa1e824c0c813ef1c5538
PRIYANSHU VERMA <priyansgood392@gmail.com>
2024-05-31 14:25:12 +0530

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,1 +1,15 @@
+#include "User.h"
+#include <iostream>
+
+User::User(const std::string& name, const std::string& email, const std::string& password)
+    : Person(name, email), password(password) {}
+
+bool User::authenticate(const std::string& password) const {
+    return this->password == password;
+}
+
+void User::display() const {
+    Person::display();
+    std::cout << "User authenticated" << std::endl;
+}
 
