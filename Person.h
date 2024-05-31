5b59a53407424a4fa9279a5d6b92f28b9b37f3d8
PRIYANSHU VERMA <priyansgood392@gmail.com>
2024-05-31 14:12:52 +0530

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,1 +1,22 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+
+class Person {
+protected:
+    std::string name;
+    std::string email;
+
+public:
+    Person(const std::string& name, const std::string& email);
+    virtual ~Person() = default;
+
+    std::string getName() const;
+    std::string getEmail() const;
+
+    virtual void display() const = 0;
+};
+
+#endif
 
