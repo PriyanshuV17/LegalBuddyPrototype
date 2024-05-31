6b0a137a056798d9d5d0cb621f73a7695bcd57de
PRIYANSHU VERMA <priyansgood392@gmail.com>
2024-05-31 14:26:28 +0530

    Update Case.cpp

--- Case.cpp
+++ Case.cpp
@@ -1,1 +1,20 @@
+#include "Case.h"
+#include <iostream>
+
+Case::Case(const std::string& caseID, const std::string& caseName) : caseID(caseID), caseName(caseName) {}
+
+std::string Case::getCaseID() const { return caseID; }
+
+std::string Case::getCaseName() const { return caseName; }
+
+void Case::addDocument(const Document& doc) {
+    documents.push_back(doc);
+}
+
+void Case::listDocuments() const {
+    std::cout << "Documents for Case: " << caseName << std::endl;
+    for (const auto& doc : documents) {
+        std::cout << doc.getTitle() << std::endl;
+    }
+}
 
